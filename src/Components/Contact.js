2228a77f563922aa2e8b844810bd2fee1816f4ac
Jatin Sonkusale <jatin22000@gmail.com>
2021-07-27 23:22:19 +0530

    className warning cleared

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,71 +3,78 @@ import "../App.scss";
 import emailjs from "emailjs-com";
 
 function Contact() {
-
   function sendEmail(e) {
     e.preventDefault();
 
-    emailjs.sendForm('service_382776l', 'template_abc6n4s', e.target, 'user_UoyXG6GIs21hFcx6zkdmI')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_382776l",
+        "template_abc6n4s",
+        e.target,
+        "user_UoyXG6GIs21hFcx6zkdmI"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
-      e.target.reset();
+        }
+      );
+    e.target.reset();
   }
 
-
   return (
-    <div class="contact">
-      <div class="container">
-        <div class="row align-items-center">
-          <div class="col-md-8">
-            <div class="contact-form">
+    <div className="contact">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-md-8">
+            <div className="contact-form">
               <form onSubmit={sendEmail}>
-                <div class="form-row">
-                  <div class="form-group col-md-6">
+                <div className="form-row">
+                  <div className="form-group col-md-6">
                     <input
                       type="text"
-                      class="form-control"
+                      className="form-control"
                       placeholder="Your Name"
                       name="name"
                     />
                   </div>
-                  <div class="form-group col-md-6">
+                  <div className="form-group col-md-6">
                     <input
                       type="email"
-                      class="form-control"
+                      className="form-control"
                       placeholder="Your Email"
                       name="email"
                     />
                   </div>
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                   <input
                     type="text"
-                    class="form-control"
+                    className="form-control"
                     placeholder="Subject"
                     name="subject"
                   />
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                   <textarea
-                    class="form-control"
+                    className="form-control"
                     rows="5"
                     placeholder="Message"
                     name="message"
                   ></textarea>
                 </div>
                 <div>
-                  <button class="btn" type="submit">
+                  <button className="btn" type="submit">
                     Send Message
                   </button>
                 </div>
               </form>
             </div>
           </div>
-          <div class="col-md-4">
-            <div class="contact-info">
+          <div className="col-md-4">
+            <div className="contact-info">
               <h3>Get in Touch</h3>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
@@ -75,29 +82,29 @@ function Contact() {
                 at dignissim lorem tempor in. Vivamus tempor hendrerit finibus.
               </p>
               <h4>
-                <i class="fa fa-map-marker"></i>123 News Street, NY, USA
+                <i className="fa fa-map-marker"></i>123 News Street, NY, USA
               </h4>
               <h4>
-                <i class="fa fa-envelope"></i>info@example.com
+                <i className="fa fa-envelope"></i>info@example.com
               </h4>
               <h4>
-                <i class="fa fa-phone"></i>+123-456-7890
+                <i className="fa fa-phone"></i>+123-456-7890
               </h4>
-              <div class="social">
+              <div className="social">
                 <a href="/contact">
-                  <i class="fab fa-twitter"></i>
+                  <i className="fab fa-twitter"></i>
                 </a>
                 <a href="/contact">
-                  <i class="fab fa-facebook-f"></i>
+                  <i className="fab fa-facebook-f"></i>
                 </a>
                 <a href="/contact">
-                  <i class="fab fa-linkedin-in"></i>
+                  <i className="fab fa-linkedin-in"></i>
                 </a>
                 <a href="/contact">
-                  <i class="fab fa-instagram"></i>
+                  <i className="fab fa-instagram"></i>
                 </a>
                 <a href="/contact">
-                  <i class="fab fa-youtube"></i>
+                  <i className="fab fa-youtube"></i>
                 </a>
               </div>
             </div>
